3b571819eddaf994ef483ccd129153b42a6484b8
Dmitry <gomerty15@mail.ru>
2021-06-15 20:09:49 +0300

    Новое решение

---
+++ trainig_2/trainig_2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+int main()
+{
+	std::vector<int> a;
+
+	int n=5;
+	int s;
+	
+	for (int i = 0; i < n; ++i) {
+		std::cin >> s;
+		a.push_back(s);
+	}
+
+	if (a[0] == a[1] == a[2] == a[3] == a[4]) {
+		std::cout << "impossible";
+	}
+
+	std::map<int, int> combinations;
+
+	for (auto x = a.begin(); x != a.end(); ++x) {
+		auto y = combinations.find(*x);
+		if (y == combinations.end()) {
+			combinations.insert(std::make_pair(*x, 1));
+		}
+		else {
+			++(y->second);
+		}
+	}
+
+	for (auto it = combinations.begin(); it != combinations.end(); ++it) {
+		std::cout << it->first << ' ' << it->second << std::endl;
+	}
+
+	//for (auto it = combinations.begin(); it != combinations.end(); ++it) {
+	//	if (it->second == 2) {
+	//		std::cout << "two pairs";
+	//	}
+	//}
+
+
+	//else {
+	//	std::cout << "nothing";
+	//}
+}
